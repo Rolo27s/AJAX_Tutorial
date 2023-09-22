b19b065a2f40038d0754a2824aadd9ac84b9f58e
FranJ <Fr4n.mlg@gmail.com>
2023-09-22 11:12:08 +0200

    Fin ajax + async-await

--- ajax.js
+++ ajax.js
@@ -117,13 +117,13 @@
 
 // Módulo 3. Fetch + Async-Await
 (() => {
-    // Instancia del objeto fetch
+    // Instancia del objeto
     const $fetchAsync = document.getElementById("fetch-async"),
     $fragment = document.createDocumentFragment();
 
     async function getData() {
         try {
-            let res = await fetch("https://jsonplaceholder.typicode.com/user"),
+            let res = await fetch("https://jsonplaceholder.typicode.com/users"),
             json = await res.json();
 
             console.log(res,json);
@@ -155,4 +155,74 @@
 
     getData();
 
+})();
+
+// Módulo 3.5 jQuery
+/* 
+    Curso de jQuery: ¿?
+    Solo servirá para mantenimiento. Ya no se desarrolla con esa API.
+*/
+
+// Módulo 4. Axios.
+(() => {
+    // Instancia del objeto axios
+    const $axios = document.getElementById("axios"),
+    $fragment = document.createDocumentFragment();
+
+    axios
+        .get("https://jsonplaceholder.typicode.com/users")
+        .then( (res) => {
+            console.log(res);
+            let json = res.data;
+
+            // Por cada elemento del objeto json vamos a crear un li con un textContent con el nombre, email y teléfono. Iremos añadiendo cada li al $fragment que es un fragmento HTML.
+            json.forEach( el => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+
+            // Cuando termine de generarse el fragmento lo insertamos en el DOM, en el id xhr del HTML
+            $axios.appendChild($fragment);
+        })
+        .catch( (err) => {
+            console.log("Estamos en el catch Axios", err.response);
+            let message = err.response.statusText || "Ocurrió un error";
+            $axios.innerHTML = `Error ${err.response.status}: ${message}`;
+        })
+        .finally(()=> {
+            console.log("Esto se ejecutará independientemente del Axios");
+        });
+})();
+
+// Módulo 5. Axios + Async-Await
+(() => {
+    // Instancia del objeto axios+AA
+    const $axiosAsync = document.getElementById("axios-async"),
+    $fragment = document.createDocumentFragment();
+
+    async function getData() {
+        try {
+            let res = await axios.get("https://jsonplaceholder.typicode.com/users"),
+            json = await res.data;
+
+            console.log(res,json);
+
+            json.forEach( el => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+
+            $axiosAsync.appendChild($fragment);
+        } catch (err) {
+            console.log("Estamos en el catch Axios+Async-Await", err.response);
+            let message = err.response.statusText || "Ocurrió un error";
+            $axiosAsync.innerHTML = `Error ${err.response.status}: ${message}`;
+        } finally {
+            console.log("Esto se ejecutará independientemente del tryCatch del Axios+Async-Await");
+        }
+    }
+
+    getData();
 })();
