9c48662013cdd4848d81cde334dfee5146a23c5a
FranJ <Fr4n.mlg@gmail.com>
2023-09-22 10:26:01 +0200

    AJAX Native JS finished

--- ajax.js
+++ ajax.js
@@ -11,10 +11,27 @@
     // 2. Asignación de eventos de la petición. ReadyStateChange detecta cualquier cambio en la petición.
     xhr.addEventListener("readystatechange", (e) => {
         // Condicional que devuelve nada hasta que el estado sea 4, que es estado completado.
+        /* 
+            Estado	                    Valor
+            READY_STATE_UNINITIALIZED	0
+            READY_STATE_LOADING	        1
+            READY_STATE_LOADED	        2
+            READY_STATE_INTERACTIVE	    3
+            READY_STATE_COMPLETE	    4
+        */
         if(xhr.readyState !== 4) return;
         // console.log(xhr);
         
         // Validación para comprobar que la respuesta del servidor sea correcta
+
+        // https://developer.mozilla.org/es/docs/Web/HTTP/Status
+        /* 
+            Respuestas informativas (100–199),
+            Respuestas satisfactorias (200–299),
+            Redirecciones (300–399),
+            Errores de los clientes (400–499),
+            y errores de los servidores (500–599).
+        */
         if(xhr.status >= 200 && xhr.status <= 299) {
             // console.log("éxito");
             // console.log(xhr.responseText);
@@ -100,5 +117,42 @@
 
 // Módulo 3. Fetch + Async-Await
 (() => {
+    // Instancia del objeto fetch
+    const $fetchAsync = document.getElementById("fetch-async"),
+    $fragment = document.createDocumentFragment();
+
+    async function getData() {
+        try {
+            let res = await fetch("https://jsonplaceholder.typicode.com/user"),
+            json = await res.json();
+
+            console.log(res,json);
+
+            // Se puede controlar el error con new Error, pero este solo admite que devuelva texto y sería interesante poder devolver un objeto para adjuntar el status y el statusText.
+            // if (!res.ok) throw new Error("Ocurrió un Error al solicitar los datos");
+
+            // Recordamos que el throw es un return que manda el flujo a nuestro catch
+            if(!res.ok) throw { status: res.status, statusText: res.statusText}
+
+            // Por cada elemento del objeto json vamos a crear un li con un textContent con el nombre, email y teléfono. Iremos añadiendo cada li al $fragment que es un fragmento HTML.
+            json.forEach( el => {
+                const $li = document.createElement("li");
+                $li.innerHTML = `${el.name} -- ${el.email} -- ${el.phone}`;
+                $fragment.appendChild($li);
+            });
+
+            // Cuando termine de generarse el fragmento lo insertamos en el DOM, en el id xhr del HTML
+            $fetchAsync.appendChild($fragment);
+        } catch (err) {
+            console.error("Estamos en el catch", err);
+            let message = err.statusText || "Ocurrió un error";
+            $fetchAsync.innerHTML = `Error ${err.status}: ${message}`;
+        } finally {
+            console.log("Estoy en el finally del Async-Await");
+        }
+
+    }
+
+    getData();
 
 })();
